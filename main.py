0a8060bf85890b577241b543660ed44c24bc218f
KW Chae <wolseo1@gmail.com>
2023-01-15 03:07:34 +0900

    feat: basic page

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import pyperclip
 
-st.text("hello streamlit!")
+title = st.text_input("Enter title", "-- Spoiler --")
+text = st.text_input("Enter text", "content")
+blank = '\u200b' * 500
+result = title + blank + text
+st.text_area("Result", result)
+
+if st.button("Copy to clipboard"):
+   pyperclip.copy(result)
