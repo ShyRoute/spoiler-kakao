2c3f7544ac66370e823281bdc6c676f66d41fa75
KW Chae <wolseo1@gmail.com>
2023-01-15 14:34:15 +0900

    feat: add title

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.markdown("### Kakaotalk Spoiler Maker")
+
 title = st.text_input("Enter title", "< Spoiler >")
 text = st.text_input("Enter text", "content")
 blank = '\u200b' * 500
